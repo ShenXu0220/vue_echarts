07e3412a6d025c62adf6edc21bfe27b8932ef643
ShenXu <sxbczyh2134@163.com>
2021-11-25 23:10:49 +0800

    横向柱状图UI样式

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -27,7 +27,7 @@
     methods:{
       // 初始化echarts
       initChart(){
-        this.chartInstance = this.$echarts.init(this.$refs.seller_ref)
+        this.chartInstance = this.$echarts.init(this.$refs.seller_ref, 'chalk')
         // 对图标对象进行鼠标事件的监听
         this.chartInstance.on('mouseover', ()=>{
           clearInterval(this.timerId)
@@ -64,6 +64,21 @@
           return item.value
         })
         const option = {
+          title:{
+            text:'▎商家销售统计',
+            textStyle:{
+              fontSize:66
+            },
+            left:20,
+            top:20
+          },
+          grid:{//这个是配置坐标轴的位置，且不包括坐标轴的文字
+            top:'20%',
+            left:'3%',
+            right:'6%',
+            bottom:'3%',
+            containLabel: true//距离是包括了坐标轴的文字
+          },
           xAxis: {
             type:'value'
           },
@@ -71,10 +86,45 @@
             type: 'category',
             data: sellerNames
           },
+          tooltip:{
+            trigger:'axis',
+            axisPointer:{
+              type:'line',
+              z:0,
+              lineStyle:{
+                width:66,
+                color:'#2D3443'
+              }
+            }
+          },
           series:[
             {
               type: 'bar',
-              data: sellerValues
+              data: sellerValues,
+              barWidth: 66,
+              label:{
+                show: true,
+                position: 'right',
+                textStyle:{
+                  color:'white'
+                }
+              },
+              itemStyle:{
+                barBorderRadius: [0, 33, 33, 0],
+                // 指明颜色渐变的方向
+                // 之命名不同百分比至下的颜色的值
+                // x1, y1, x2, y2   
+                color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0,[
+                  {//这个是0%状态下的颜色值
+                    offset:0,
+                    color:'#5052EE'
+                  },
+                  {//这个是100%状态下的颜色值
+                    offset:1,
+                    color:'#AB6EE5'
+                  }
+                ])
+              }
             }
           ]
         }
